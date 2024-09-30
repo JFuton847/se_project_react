839ddb3f8ced08363c0f80df1b71755d73766814
JFuton847 <jimmyfuton847@gmail.com>
2024-09-30 14:25:26 -0600

    move index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import "../src/pages/index.css";
+import "../src/blocks/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
