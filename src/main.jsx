77667b6367e11cc059df35694c038631e95efab4
JFuton847 <jimmyfuton847@gmail.com>
2024-09-30 14:23:24 -0600

    finish fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import "./blocks/index.css";
+import "../src/pages/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
