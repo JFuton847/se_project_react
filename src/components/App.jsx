7798b98317e7ae9ae47aab5155444e3a276b3791
JFuton847 <jimmyfuton847@gmail.com>
2024-09-30 10:47:50 -0600

    complete footer, add favicon, start styling modal form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ import Main from "./Main";
 import ModalWithForm from "../components/ModalWithForm";
 import ItemModal from "../components/ItemModal";
 import { getWeather, filterWeatherData } from "../utils/weatherApi";
+import Footer from "./Footer";
 
 function App() {
   const [weatherData, setWeatherData] = useState({
@@ -93,6 +94,7 @@ function App() {
         card={selectedCard}
         onClose={closeActiveModal}
       />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,13 @@
+import "../blocks/footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <p className="footer__text">Developed by James Petersen</p>
+        <span>2024</span>
+      </div>
+    </footer>
+  );
+}
 export default Footer;
