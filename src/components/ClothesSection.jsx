eec08b61b0c69e2647389f07a7bdfcfe8dbf3f6e
JFuton847 <jimmyfuton847@gmail.com>
2024-10-14 19:06:06 -0500

    pass props to onCardClick in ClothesSection.jsx and get it functioning

--- src/components/ClothesSection.jsx
+++ src/components/ClothesSection.jsx
@@ -2,22 +2,17 @@ import { defaultClothingItems } from "../utils/constants.js";
 import ItemCard from "../components/ItemCard";
 import "../blocks/ClothesSection.css";
 
-function ClothesSection() {
+function ClothesSection({ onCardClick }) {
   return (
     <div className="clothes-section">
       <div className="clothes-section__top">
         <p className="clothes-section__header">Your items</p>
         <button className="clothes-section__btn">+ Add New</button>
       </div>
-      <ul className="cards__list">
+      <ul className="clothes-section__items">
         {defaultClothingItems.map((item) => {
           return (
-            <ItemCard
-              key={item._id}
-              item={item}
-              // To Do - pass as prop
-              // onCardClick={handleCardClick}
-            />
+            <ItemCard key={item._id} item={item} onCardClick={onCardClick} />
           );
         })}
       </ul>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,14 +2,14 @@ import SideBar from "../components/SideBar";
 import ClothesSection from "../components/ClothesSection";
 import "../blocks/Profile.css";
 
-function Profile() {
+function Profile({ onCardClick }) {
   return (
     <div className="profile">
       <section className="profile__sidebar">
         <SideBar />
       </section>
       <section className="profile__clothing-items">
-        <ClothesSection />
+        <ClothesSection onCardClick={onCardClick} />
       </section>
     </div>
   );
